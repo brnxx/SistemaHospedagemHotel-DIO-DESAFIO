bc95d43f72ac9aed76f42df4e3c29bb6c5882fcf
Bruno Monteiro <bruno-moonteiro@hotmail.com>
2024-02-16 11:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaHospedagemHotel/Hotel/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SistemaHospedagemHotel.Hotel
+{
+    internal class Pessoa
+    {
+
+        private string _nome;
+        private string _sobrenome;
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+
+        }
+
+        public string Nome
+        {
+            get => _nome.ToUpper();
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+
+        public string Sobrenome
+        {
+            get => _sobrenome;
+
+            set
+            {
+
+                if (value == "")
+                {
+                    throw new ArgumentException("O sobrenome não pode ser vazio");
+                }
+            }
+        }
+
+        public void ApresentarPessoa()
+        {
+            Console.WriteLine($"Nome e sobrenome: {Nome} {Sobrenome}");
+        }
+
+    }
+}
+
---
+++ SistemaHospedagemHotel/Program.cs
@@ -0,0 +1,16 @@
+﻿using SistemaHospedagemHotel.Hotel;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+
+    }
+
+}
+
